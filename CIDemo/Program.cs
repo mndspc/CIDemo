c5f25af79fc8a2729c7044c6d4e01ff2d77550a8
Shoaib <shoaib.ghachi@gmail.com>
2022-06-05 20:17:05 +0530

    Add project files.

---
+++ CIDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIDemo
+{
+    public class Program
+    {
+       public static string Message()
+        {
+         return "Hello World";
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using CIDemo;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World";
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+               string actuall= CIDemo.Program.Message();
+            Assert.AreEqual(Expected, actuall);
+            
+        }
+    }
+}
